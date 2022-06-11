3fd29b9fad759a2e456e505f1d21deaf2d855ac0
lMatu <lMatu.pereiramatin@gmail.com>
2022-06-11 19:14:54 -0300

    Estetica casi lista

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,6 +1,8 @@
 //Libraries
 import React from 'react'
 import styled from 'styled-components'
+import { Link } from "react-router-dom";
+import { MdKeyboardArrowRight } from 'react-icons/md';
 
 //Other components
 import Device from '../Common/Device'
@@ -34,14 +36,12 @@ const Container = styled.div`
   padding 1rem;
 `
 
-const Row = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between
+const Box = styled.div`
+  display: flex;;
+  flex-direction: column;
   width: 100%;
   margin: auto;
-  border: 1px solid #ccc;
-  padding: 1rem 0;
+  border: 1px solid #efefef;
   border-radius: 8px;
   box-shadow: 0px 3px 4px rgba(0, 0, 0, 0.25);
   @media only screen and ${Device.xs} {
@@ -49,31 +49,105 @@ const Row = styled.div`
   }
 `
 
-const Items = styled.div`
-  #flex: 3;
-  width: 2rem;
+const Content = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  width: 100%;
   margin: auto;
-  border: 1px solid #ccc;
+`
+
+const Items = styled(Link)`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  flex: 3 3 3 3;
+  text-decoration: none;
+  color: #FFFFFF;
+  width: 30%;
+  max-height: 5rem;
+  margin: 1.5rem;
+  border: 1px solid #efefef;
   padding: 1rem 0;
   border-radius: 8px;
-  box-shadow: 0px 3px 4px rgba(0, 0, 0, 0.25);
+  box-shadow: 0px 3px 4px rgba(240, 240, 240, 0.15);
+  &:hover{
+    background-color: #054d6e;    
+    box-shadow: 0px 3px 4px rgba(180, 180, 180, 0.25);
+  }
+  @media only screen and ${Device.md} {
+    width: 40%;
+  }
+  @media only screen and ${Device.sm} {
+    width: 100%;
+  }
   @media only screen and ${Device.xs} {
-    #flex: 1;
+    width: 100%;
   }
 `
+const ItemsCol = styled.div`
+  padding: 0.5rem;
+  @media only screen and ${Device.xs} {
+    width: 100%;
+  }
+`
+
+const ImgProfile = styled.img`
+  width: 5rem;
+  height: auto;
+`
+
+const ArrowRight = styled(MdKeyboardArrowRight)`
+  color: white;
+  margin: 0 auto;
+  font-size: 1.3rem;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const Group = styled.div`
+  display: flex;
+  margin: 1rem;
+  flex-flow: row wrap;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Input = styled.input`
+  @media only screen and ${Device.xs} {
+    margin: 0.5rem 0;
+    width: 100%;
+  }
+`;
+
+const Button = styled.button`
+  &:hover {
+    background-color: #054d6e;
+    color: #FFFFFF;
+    border-color: #FFFFFF;
+  }
+  @media only screen and ${Device.xs} {
+    width: 100%;
+  }
+`;
 
 function Users() {
   return (
     <Container>
       <Logo src={logo} alt="logo" />
-      <Row>
-        <Items></Items>
-        <Items></Items>
-        <Items></Items>
-        <Items></Items>
-        <Items></Items>
-        <Items></Items>
-      </Row>
+      <Box>
+        <Group>
+          <Input></Input><Button type="button">Buscar</Button>
+        </Group>
+        <Content>
+          <Items to={`/profile/1`} key={1}>
+            <ItemsCol><ImgProfile src={logo}></ImgProfile></ItemsCol>
+            <ItemsCol>Matias Pereira</ItemsCol>
+            <ItemsCol><ArrowRight /></ItemsCol>
+          </Items>
+        </Content>
+      </Box>
     </Container>
   )
 }
